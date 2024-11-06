0eb475aed1745f3629c47d59341d2c52db2094a2
Anos000 <144375421+Anos000@users.noreply.github.com>
2024-11-06 11:03:37 +0300

    Update test_sql_parc_mysqlV2.py

--- test_sql_parc_mysqlV2.py
+++ test_sql_parc_mysqlV2.py
@@ -7,7 +7,7 @@ from datetime import datetime
 import pytz
 import re
 import time
-import csv
+
 # Настройка для работы с Chrome
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')  # Запуск браузера в фоновом режиме
@@ -177,33 +177,7 @@ if today_data:
 def ensure_connection():
     if not conn.is_connected():
         conn.reconnect()
-# Функция для чтения данных из таблицы и сохранения их в CSV
-def export_table_to_csv(table_name, filename, headers):
-    # Обеспечиваем подключение к базе данных
-    ensure_connection()
-    cursor.execute(f"SELECT * FROM {table_name}")
-    rows = cursor.fetchall()
-    
-    # Сохранение данных в CSV
-    with open(filename, mode='w', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        writer.writerow(headers)  # Записываем заголовки
-        writer.writerows(rows)    # Записываем все строки таблицы
-    print(f"Данные из таблицы '{table_name}' сохранены в {filename}")
 
-# Сохранение всех данных из таблицы productsV3 в productsV3.csv
-export_table_to_csv(
-    'productsV2', 
-    'productsV2.csv', 
-    ['ID', 'Дата парсинга', 'Название', 'Артикул', 'Цена', 'Изображение', 'Ссылка']
-)
-
-# Сохранение актуальных данных из таблицы today_productsV3 в today_productsV3.csv
-export_table_to_csv(
-    'today_productsV2', 
-    'today_productsV2.csv', 
-    ['ID', 'Дата парсинга', 'Название', 'Артикул', 'Цена', 'Изображение', 'Ссылка']
-)
 
 # Сохранение изменений и закрытие соединения
 conn.commit()
