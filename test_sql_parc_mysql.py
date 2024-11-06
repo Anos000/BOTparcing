a9784122906e7f731b0fa88d1d75d72c9c80cd56
Anos000 <144375421+Anos000@users.noreply.github.com>
2024-11-06 11:01:54 +0300

    Update test_sql_parc_mysql.py

--- test_sql_parc_mysql.py
+++ test_sql_parc_mysql.py
@@ -7,7 +7,7 @@ from mysql.connector import Error
 from datetime import datetime
 import pytz
 import re
-import csv
+
 # Настройка для работы с Chrome
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
@@ -159,33 +159,6 @@ cursor.executemany('''
 ''', today_data)
 
 
-# Функция для чтения данных из таблицы и сохранения их в CSV
-def export_table_to_csv(table_name, filename, headers):
-    # Обеспечиваем подключение к базе данных
-    ensure_connection()
-    cursor.execute(f"SELECT * FROM {table_name}")
-    rows = cursor.fetchall()
-    
-    # Сохранение данных в CSV
-    with open(filename, mode='w', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        writer.writerow(headers)  # Записываем заголовки
-        writer.writerows(rows)    # Записываем все строки таблицы
-    print(f"Данные из таблицы '{table_name}' сохранены в {filename}")
-
-# Сохранение всех данных из таблицы productsV3 в productsV3.csv
-export_table_to_csv(
-    'products', 
-    'products.csv', 
-    ['ID', 'Дата парсинга', 'Название', 'Артикул', 'Цена', 'Изображение', 'Ссылка']
-)
-
-# Сохранение актуальных данных из таблицы today_productsV3 в today_productsV3.csv
-export_table_to_csv(
-    'today_products', 
-    'today_products.csv', 
-    ['ID', 'Дата парсинга', 'Название', 'Артикул', 'Цена', 'Изображение', 'Ссылка']
-)
 
 # Сохранение и закрытие соединения
 conn.commit()
